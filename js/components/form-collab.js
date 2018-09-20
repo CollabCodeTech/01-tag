4e7827e183d9de8bb5fae0578c82539ab953bdf6
Marco Bruno <marco.bruno.br@gmail.com>
2018-09-20 02:20:06 +0000

    Feito a tag dinamicamente mas toda vez que adicinamos uma nova tag todas elas vibram locuamente

--- js/components/form-collab.js
+++ js/components/form-collab.js
@@ -6,12 +6,14 @@
   2 - Criar um ouvindo para o evento KeyUp ou KeyPress ou KeyDown (OK)
   3 - Dentro do evento precisamos descobrir quando o usuário aperta a tecla ENTER (OK)
   4 - Quando usuário apertar o ENTER nós queremos pegar o texto (OK)
+  5 - Criar o component tag com o texto dentro como valor e usar o id e for
 */
 "use strict";
 
 const $formCollab = window.document.querySelector(".form-collab");
 const $label = $formCollab.querySelector(".label");
 const $input = $formCollab.querySelector(".input");
+const $tags = $formCollab.querySelector(".tags");
 
 const focusAndBlur = () => $label.classList.toggle("-focus");
 
@@ -23,17 +25,20 @@ $input.addEventListener("blur", focusAndBlur);
 $input.addEventListener("keyup", event => {
   event.preventDefault();
 
-  // const keyCode = event.keyCode;
-  // const key = event.key;
-
   const { keyCode } = event;
   const keys = {
     13: "ENTER"
   };
+  const template =
+    '<input class="tagInput" id="frontend" type="checkbox"><label class="tag" for="frontend">FrontEnd</label>';
 
   if (keys[keyCode] === "ENTER") {
     const { value } = $input;
+    const template = `
+      <input class="tagInput" id="${value}" type="checkbox" checked>
+      <label class="tag" for="${value}">${value}</label>
+    `;
 
-    console.log(value);
+    $tags.innerHTML = template + $tags.innerHTML;
   }
 });
